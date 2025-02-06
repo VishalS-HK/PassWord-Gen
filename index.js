309bf495c79840bffc03a46b65478a92cd61db0e
Vishal S <vishals@Vishals-MacBook-Air-M1.local>
2025-02-06 22:27:02 +0530

    Modified: Added two kinds of functions, one for strong password, and the other to generate without special characters

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 let passwordEl1 = document.getElementById("password-box-1");
 let passwordEl2 = document.getElementById("password-box-2");
 
-function generatePassword() {
-    let pass1 = "";
+function withoutSpecialCharacters() {
     let pass2 = "";
-    for(let i = 0; i < 16; ++i) {
-        pass1 += characters[Math.floor(Math.random() * characters.length)];
-    }
 
     for(let i = 0; i < 16; ++i) {
         pass2 += characters[Math.floor(Math.random() * characters.length)];
     }
+    return pass2;
+}
 
+function strongPassword() {
+    let parts = [];
+    for(let i = 0; i < 3; ++i) {
+        let part = "";
+        for(let j = 0; j < 6; ++j) {
+            part += characters[Math.floor(Math.random() * characters.length)];
+        }
+        parts.push(part);
+    }
+    return parts.join("-");
+}
+
+function generatePassword() {
+    let pass1 = strongPassword();
+    let pass2 = withoutSpecialCharacters();
     passwordEl1.textContent = pass1;
     passwordEl2.textContent = pass2;
 }
